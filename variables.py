3e27bc0019bbd43df7860ddd11ed803e9afb717b
ThatAgricBoy <samueljohn3999@gmail.com>
2023-03-02 16:48:17 +0100

    using str, input and the print function

--- variables.py
+++ variables.py
@@ -16,3 +16,11 @@ print('What is your age? ')
 myAge = input()
 
 print('You will be ' + str(int(myAge) + 1) + ' in a year')
+
+# This line of code gets input from user
+# then finds the length of the input
+# because we can only add strings to strings we have
+# to use the str function to add the result
+# then it uses the len function to calculate the
+# the length of input from the user
+print("The length of your name is " + str(len(input("Enter your second name: "))))
