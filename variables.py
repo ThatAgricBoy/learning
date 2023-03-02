f6bf6cff51c707c772b6e61078779081afe5f4d6
Thatagricguy <samueljohn3999@gmail.com>
2023-03-02 06:29:21 +0000

    using variables

---
+++ variables.py
@@ -0,0 +1,17 @@
+# This programs says hello world and ask for your name
+
+print('Hello World')
+
+myName = input('My name is: ')
+
+print('It is good to meet you, ' + myName)
+
+print('The length of your name is: ')
+
+print(len(myName))
+
+print('What is my age? ')
+
+myAge = input()
+
+print('You will be ' + str(it(myAge) + 1) + 'in a year')
