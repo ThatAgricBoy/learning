350e3ec55a836ab9b30cddd0bf68518c3ccb0563
Thatagricguy <samueljohn3999@gmail.com>
2023-03-02 18:58:29 +0000

    control

---
+++ control.py
@@ -0,0 +1,18 @@
+name = ''
+age =''
+name = input('What is your name: ')
+age = input('Enter age: ')
+if name == 'Alice':
+	print('Hi Alice')
+elif age < 12:
+	print('You are not a Alice Kiddo')
+elif age > 2000:
+	print('Unlike you, Alice is not an undead, immortal vampire')
+elif age > 100:
+	print('You are not Alice, grannie.')
+
+#name = ''
+#while name != 'your name':
+# print('Please type your name.')
+# name = input()
+#print('Thank you!')
