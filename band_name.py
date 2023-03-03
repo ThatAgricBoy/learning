d4a3f842141654c71d0dfe31275e8d744ae2b753
Samuel John <117738855+ThatAgricBoy@users.noreply.github.com>
2023-03-03 10:11:01 +0000

    Band name project

---
+++ band_name.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator\n")
+city_name = input("What's name of the city ou grew up in")
+pet_name = input("What's your pet's name")
+print("city_name" + "pet_name")
