c3e2e4a9008a3ec92145c117ec17d40b3e44d76c
Thatagricguy <samueljohn3999@gmail.com>
2023-03-08 20:11:21 +0000

    love

--- love_calculator.py
+++ love_calculator.py
@@ -12,8 +12,9 @@ name2 = input("What is their name? \n")
 mystr = name1 + name2
 true = mystr.lower().count('t') + mystr.lower().count('r') + mystr.lower().count('u') + mystr.lower().count('e')
 
-false =  mystr.lower().count('f') + mystr.lower().count('a') + mystr.lower().count('l') + mystr.lower().count('s') + mystr.lower().count('e')
+false =  mystr.lower().count('l') + mystr.lower().count('o') + mystr.lower().count('v') + mystr.lower().count('e')
 
-score = str(true) + str(false)
-print(score)
+score = true + false
 
+if score < 10 or score  > 90:
+	print(f"Your score is {score}, you go together like coke and mentos.")
