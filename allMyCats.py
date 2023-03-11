8b8857acf79e2ed060e0f3f6c429c4d688c37544
ThatAgricBoy <samueljohn3999@gmail.com>
2023-03-11 18:40:37 +0100

    all my cats

--- allMyCats.py
+++ allMyCats.py
@@ -1,10 +1,10 @@
 catNames = []
 while True:
-	print('Enter the name of cat ' + str(len(catNames) + 1 + ' (Or enter nothing to stop): ')
+	print('Enter the name of cat ' + str(len(catNames) + 1) + ' (Or enter nothing to stop): ')
 	name = input()
 	if name == '':
 		break
-	catNames = catNames + []
+	catNames = catNames + [name]
 print('The cat names are: ')
 for name in catNames:
 	print(' ' + name)
