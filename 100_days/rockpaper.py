23d6aa2a8aba93c790fa1c1d53c6453c783714d1
ThatAgricBoy <samueljohn3999@gmail.com>
2023-03-17 17:07:23 -0700

    rock paper scissors

--- 100_days/rockpaper.py
+++ 100_days/rockpaper.py
@@ -39,38 +39,38 @@ random_choice = random.randint(0, Turn2 - 1)
 compTurn = gamePlay[random_choice]
 
 if myTurn == 'rock' and compTurn == 'rock':
-    print(myTurn)
-    print(compTurn)
+    print(rock)
+    print(rock)
     print("Draw")
 elif myTurn == 'rock' and compTurn == 'paper':
-    print(myTurn)
-    print(compTurn)
+    print(rock)
+    print(paper)
     print("You lose: Paper beats rock")
 elif myTurn == 'rock' and compTurn == 'scissors':
-    print(myTurn)
-    print(compTurn)
+    print(rock)
+    print(scissors)
     print("You Win: Rock beats scissors")
 elif myTurn == 'paper' and compTurn == 'rock':
-    print(myTurn)
-    print(compTurn)
+    print(paper)
+    print(rock)
     print("You Win: Paper beats rock")
 elif myTurn == 'paper' and compTurn == 'paper':
-    print(myTurn)
-    print(compTurn)
+    print(paper)
+    print(paper)
     print("Draw")
 elif myTurn == 'paper' and compTurn == 'scissors':
-    print(myTurn)
-    print(compTurn)
+    print(paper)
+    print(scissors)
     print("You lose: Scissors beats paper")
 elif myTurn == 'scissors' and compTurn == 'rock':
-    print(myTurn)
-    print(compTurn)
+    print(scissors)
+    print(rock)
     print("You lose: Rock beats scissors")
 elif myTurn == 'scissors' and compTurn == 'paper':
-    print(myTurn)
-    print(compTurn)
+    print(scissors)
+    print(paper)
     print("You Win: Scissors beats paper")
 elif myTurn == 'scissors' and compTurn == 'scissors':
-    print(myTurn)
-    print(compTurn)
+    print(scissors)
+    print(scissors)
     print("Draw")
