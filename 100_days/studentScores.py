caa970a6336743ef7e458b2950fe114b79c6f552
Thatagricguy <samueljohn3999@gmail.com>
2023-03-21 23:25:17 +0000

    student

--- 100_days/studentScores.py
+++ 100_days/studentScores.py
@@ -1,8 +1,12 @@
-# 🚨 Don't change the code below 👇
+#  Don't change the code below
 student_scores = input("Input a list of student scores ").split()
-for score in range(0, len(student_scores)):
-  student_scores[n] = int(student_scores[n])
+highest = 0
+for n in range(0, len(student_scores)):
+	student_scores[n] = int(student_scores[n])
+	if student_scores[n] > highest:
+	highest = student_scores[n]
 print(student_scores)
+print(highest)
 # 🚨 Don't change the code above 👆
 
 #Write your code below this row 👇
