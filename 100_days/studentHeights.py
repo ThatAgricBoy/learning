01f264a415f7286c7794c0502ff99547dd35e9bf
ThatAgricBoy <samueljohn3999@gmail.com>
2023-03-21 06:59:23 -0700

    students heights

--- 100_days/studentHeights.py
+++ 100_days/studentHeights.py
@@ -2,16 +2,18 @@
 
 # 🚨 Don't change the code below 👇
 total = 0
+count = 0
 student_heights = input("Input a list of student heights ").split()
 
 for n in range(0, len(student_heights)):
-	student_heights[n] = int(student_heights[n])
+    student_heights[n] = int(student_heights[n])
 # 🚨 Don't change the code above 👆
 
 
 #Write your code below this row 👇
-	total += student_heights[n]
-	avg = total / student_heights[n]
+    total += student_heights[n]
+    count += 1
+    avg = total / count
 print(student_heights)
 print(total)
-print(avg)
+print(round(avg, 2))
