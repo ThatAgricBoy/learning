d6c73b689c4d57e210bc1b77eaf37a1ac3e3459c
Thatagricguy <samueljohn3999@gmail.com>
2023-03-23 04:21:07 +0000

    first test

--- 100_days/passwordGenerator.py
+++ 100_days/passwordGenerator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-Password Generator Project
+# Password Generator Project
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -17,3 +17,8 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
+passWord = [nr_letters, nr_symbols, nr_numbers]
+for password in range(0, len(passWord)):
+		print(passWord)
+
