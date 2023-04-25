9ba841f68a811104085f824fb6190a4e9054922d
ThatAgricBoy <samueljohn3999@gmail.com>
2023-04-25 23:30:22 +0100

    graphics

--- zelle/first_graphics_program.py
+++ zelle/first_graphics_program.py
@@ -1,6 +1,37 @@
 #!/usr/bin/python3
 
 
-import graphics
+from graphics import *
 
-win = graphics.GraphWin()
+
+win = GraphWin('Shapes')
+
+center = Point(100, 100)
+circ = Circle(center, 30)
+circ.setFill('red')
+circ.draw(win)
+
+label = Text(center, "Red Circle")
+label.draw(win)
+
+rect = Rectangle(Point(30,30), Point(70, 70))
+rect.draw(win)
+
+line = Line(Point(20, 30), Point(180, 165))
+line.draw(win)
+
+oval = Oval(Point(20, 150), Point(180, 199))
+oval.draw(win)
+
+
+while True:
+	# Do any drawing or interaction with the window here
+	# ..
+	# Wait for the user to click the close button
+	if win.checkMouse() is None:
+		continue
+	else:
+		break
+
+
+win.close()
