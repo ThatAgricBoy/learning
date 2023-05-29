59fdd9b331327baff4b04c1619f4128c5ec390d0
Thatagricguy <samueljohn3999@gmail.com>
2023-05-29 22:36:51 +0100

    food class

--- python/100_days/snake_game/main.py
+++ python/100_days/snake_game/main.py
@@ -1,19 +1,28 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
+from food import Food
 import time
+
 window = Screen()
 window.title("Snake Game")
 window.setup(width=600, height=600)
 window.bgcolor("black")
 window.tracer(0)
 
+snake = Snake()
+food = Food()
+
+window.listen()
+window.onkey(snake.up, "Up")
+window.onkey(snake.right, "Right")
+window.onkey(snake.left, "Left")
+window.onkey(snake.down, "Down")
 
 game_is_on = True
 while game_is_on:
     window.update()
     time.sleep(0.1)
-
-
-
+    snake.move()
 
 
 
--- python/100_days/snake_game/snake.py
+++ python/100_days/snake_game/snake.py
@@ -1,24 +1,45 @@
 from turtle import Turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 class Snake:
     """This module describes the behaviour of the snake"""
-    turtle_positions = [(0, 0), (-20, 0), (-40, 0)]
-    snakes = []
-    for position in turtle_positions:
-        segment = Turtle("square")
-        segment.color("white")
-        segment.penup()
-        segment.goto(position)
-        snakes.append(segment)
-    def __init__(self, ):
-        pass
+    def __init__(self):
+        self.snakes = []
+        self.create_snakes()
+        self.head = self.snakes[0]
+
+    def create_snakes(self):
+        for position in STARTING_POSITIONS:
+            segment = Turtle("square")
+            segment.color("white")
+            segment.penup()
+            segment.goto(position)
+            self.snakes.append(segment)
 
     def move(self):
+        for seg_num in range(len(self.snakes) - 1, 0, -1):
+            new_x = self.snakes[seg_num - 1].xcor()
+            new_y = self.snakes[seg_num - 1].ycor()
+            self.snakes[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
 
-        for seg_num in range(len(snakes) - 1, 0, -1):
-            new_x = snakes[seg_num - 1].xcor()
-            new_y = snakes[seg_num - 1].ycor()
-            snakes[seg_num].goto(new_x, new_y)
-        snakes[0].forward(20)
+    def up(self):
+        if self.head.setheading() != DOWN:
+            self.head.setheading(UP)
+    def down(self):
+        if self.head.setheading() != UP:
+            self.head.setheading(DOWN)
 
+    def left(self):
+        if self.head.setheading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.setheading() != LEFT:
+            self.head.setheading(RIGHT)
 
 
