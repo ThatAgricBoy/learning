7cd8bea133a862ff0c1103187393dbe4c785be95
Thatagricguy <samueljohn3999@gmail.com>
2023-05-30 17:56:14 +0100

    added the pad class

--- python/100_days/ping_pong_game/main.py
+++ python/100_days/ping_pong_game/main.py
@@ -1,28 +1,25 @@
-from turtle import Screen, Turtle
-# from paddle import pad
+from turtle import Screen
+from paddle import Pad
 
 window = Screen()
 window.setup(width=800, height=600)
 window.bgcolor("black")
 window.title("Ping Pong Game")
+window.tracer(0)
 
-pad = Turtle()
-pad.shape("square")
-pad.color("white")
-pad.shapesize(stretch_wid=5, stretch_len=1)
-pad.penup()
-pad.goto(350, 0)
+right_pad = Pad((350, 0))
+left_pad = Pad((-350, 0))
 
-def go_up():
-    new_y = pad.ycor() + 20
-    pad.goto(pad.xcor(), new_y)
 
-def go_down():
-    new_y = pad.ycor() - 20
-    pad.goto(pad.xcor(), new_y)
 
 window.listen()
-window.onkey(go_up, "Up")
-window.onkey(go_down, "Down")
+window.onkey(right_pad.go_up, "Up")
+window.onkey(right_pad.go_down, "Down")
+window.onkey(left_pad.go_up, "w")
+window.onkey(left_pad.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    window.update()
 
 window.exitonclick()
--- python/100_days/ping_pong_game/paddle.py
+++ python/100_days/ping_pong_game/paddle.py
@@ -1,5 +1,17 @@
-# from turtle import Turtle
-# class Pad(Turtle):
-#     self.__init()
-#     super()__in
-#     pad = Turtle("rectangle")
+from turtle import Turtle
+class Pad(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
