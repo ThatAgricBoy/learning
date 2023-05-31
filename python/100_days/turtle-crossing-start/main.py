affdf45ce9e384dfa7b6d6cd2566fa40d6439362
Thatagricguy <samueljohn3999@gmail.com>
2023-05-31 09:25:43 +0100

    added scoreboard class

--- python/100_days/turtle-crossing-start/main.py
+++ python/100_days/turtle-crossing-start/main.py
@@ -10,9 +10,11 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
@@ -20,5 +22,15 @@ while game_is_on:
 
     car_manager.create_car()
     car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.speed_up()
+        scoreboard.increase_level()
 
 screen.exitonclick()
