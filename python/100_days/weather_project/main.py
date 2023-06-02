c18e1f699ea336dd4e6f607e2f0a2268da873713
Thatagricguy <samueljohn3999@gmail.com>
2023-06-02 04:26:01 +0100

    using pandas

--- python/100_days/weather_project/main.py
+++ python/100_days/weather_project/main.py
@@ -19,4 +19,28 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+
+data_dict = data.to_dict()
+temperature_list = data["temp"].to_list()
+
+"""finding the mean using python"""
+average_temp = sum(temperature_list)/len(temperature_list)
+print(round(average_temp, 2))
+
+"""finding the mean using pandas"""
+print(data["temp"].mean())
+print(data["temp"].max())
+
+"""getting the temperature of a particular day"""
+print(data[data.day == "monday"])
+
+"""getting the maximum temp for the week"""
+print(data[data.temp == data["temp"].max()])
+
+"""creating a data frame from scratch"""
+data_dict = {
+    "students": ["Ayo", "Seyi", "Max"],
+    "scores": [78, 85, 90]
+}
+data = pandas.DataFrame(data_dict)
+data.to_csv("data.csv")
