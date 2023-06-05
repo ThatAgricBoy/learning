8d42a2e6c970917c817c4e69148bae10e8f82b99
Thatagricguy <samueljohn3999@gmail.com>
2023-06-05 16:23:16 +0100

    added sound to notify the end of a rep

--- python/100_days/GUI/pomodoro_alarm/main.py
+++ python/100_days/GUI/pomodoro_alarm/main.py
@@ -1,14 +1,13 @@
 from tkinter import *
 import math
-from pydub import AudioSegment
-from pydub.playback import play
+import winsound
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1
+WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
@@ -16,9 +15,9 @@ timer = None
 
 #-----------------------------SOUND-----------------------------------------#
 def play_sound():
-    sound = AudioSegment.from_file('notification.wav', format='wav')
-    play(sound)
+    winsound.PlaySound("notification.wav", winsound.SND_FILENAME)
 # ---------------------------- TIMER RESET ------------------------------- # 
+
 def reset_timer():
     window.after_cancel(timer)
     canvas.itemconfig(timer_text, text="00:00")
@@ -27,7 +26,9 @@ def reset_timer():
     global reps
     reps = 0
 
+
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+
 def start_timer():
     global reps
     reps += 1
@@ -45,12 +46,16 @@ def start_timer():
     else:
         count_down(work_sec)
         title_label.config(text="Work", fg=GREEN)
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
+
     count_min = math.floor(count / 60)
     count_sec = count % 60
-    if count_sec == 0:
+    if count_sec < 10:
         count_sec = f"0{count_sec}"
+
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         global timer
@@ -63,13 +68,15 @@ def count_down(count):
             marks += "✅"
         check_marks.config(text=marks)
         play_sound()
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
-window.title("Pomodoro Alart")
+window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
 
-title_label= Label(text="Timer", fg=GREEN, bg= YELLOW, font=(FONT_NAME, 50))
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
 title_label.grid(column=1, row=0)
 
 hard_thing_label = Label(text="Do Hard Things", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 30))
@@ -77,20 +84,33 @@ hard_thing_label.grid(column=1, row=4)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(99, 112, image=tomato_img)
+canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-
-start_button = Button(text="Start", bg=YELLOW, font=(FONT_NAME, 15), highlightthickness=0, command=start_timer)
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", bg=YELLOW, font=(FONT_NAME, 15), highlightthickness=0, command=reset_timer)
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
-check_marks = Label(fg= GREEN, bg=YELLOW)
+check_marks = Label(fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=3)
+
+
+
+
+
 
 window.mainloop()
 
-## This is just a comment
+
+
+
+
+
+
+
+
+
+
