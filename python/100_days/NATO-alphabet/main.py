2aaaabc75bbeddf7e39fce88a5386d8683596018
Thatagricguy <samueljohn3999@gmail.com>
2023-06-06 12:32:39 +0100

    added error handling

--- python/100_days/NATO-alphabet/main.py
+++ python/100_days/NATO-alphabet/main.py
@@ -7,11 +7,16 @@ phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 game_on = True
 while game_on:
     word = input("Enter a word: ").upper()
-    output_list = [phonetic_dict[letter] for letter in word]
-    print(output_list)
-    new_word = input("Do you have more words to check?: Yes, No: ").upper()
-    if new_word == "NO":
-        game_on = False
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Enter only letters of the alphabet")
+    else:
+        output_list = [phonetic_dict[letter] for letter in word]
+        print(output_list)
+        new_word = input("Do you have more words to check?: Yes, No: ").upper()
+        if new_word == "NO":
+            game_on = False
 
 
 
