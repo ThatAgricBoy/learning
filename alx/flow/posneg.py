5a4d416283fe329bce76c6da19e598ac79f4a97d
Thatagricguy <samueljohn3999@gmail.com>
2023-06-08 18:00:09 +0100

    peer learning on control statements

---
+++ alx/flow/posneg.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if number > 0:
+    print(f"{number} is positive")
+elif number < 0:
+    print(f"{number} is negative")
+else:
+    print(f"{number} is zero")
+
+#top to bottom
+print("this is python")
+print("thi is c")
+print("this is Java")
