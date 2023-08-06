55916a492df0e6f9b40c75e406e01cf13e180dbc
ThatAgricBoy <samueljohn3999@gmail.com>
2023-08-06 14:42:42 +0100

    taskly

--- python/automate/odd_even.py
+++ python/automate/odd_even.py
@@ -1,8 +1,8 @@
 # This program checks if a number is even or odd
-
-number = int(input("Enter a number "))
-
-if number % 2 == 0:
- print("This is an even number.")
-else:
- print("This is an odd number")
+#
+# number = int(input("Enter a number "))
+#
+# if number % 2 == 0:
+#  print("This is an even number.")
+# else:
+#  print("This is an odd number")
---
+++ python/simple_webserver/simple_webserver.py
@@ -0,0 +1,5 @@
+# #!/usr/bin/python3
+# # A simple webserver
+# import BaseHTTPServer
+#
+# class RequestHandler(BaseHTTPServer.Ba)
